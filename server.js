26a6bd6a00fc0417e5bf248da69563816e13e3fd
Mark <mark.e.eaton@gmail.com>
2018-12-01 09:07:01 -0600

    updated plist

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-require('dotenv').config()
 const config = require('./config');
-const express = require('express')
+const express = require('express');
 const app = express()
 
 const port = config.port;
 
-app.get('/', (req, res) => res.send('Hello World! test'))
+app.get('/', (req, res) => {
+    console.log("get '/'");
+    res.send('Hello World!')
+});
+
 app.post('/', (req, res) => {
     console.log('Hi From GitHub')
     console.log(req);
     res.sendStatus(200);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port), () => {
+    console.log(`Example app listening on port ${port}!`);
+}
